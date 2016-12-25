14bf770399aecb2c3c80a38a1eb95d43a91bf1fa
binod <binodnp@outlook.com>
2016-12-26 04:03:03 +0545

    Routine development.

--- Controllers/Tasks/AttendanceController.cs
+++ Controllers/Tasks/AttendanceController.cs
@@ -1,13 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Frapid.ApplicationState.Cache;
 using Frapid.Areas.Authorization;
+using Frapid.Areas.CSRF;
 using Frapid.Dashboard;
 using Frapid.Dashboard.Controllers;
 using MixERP.HRM.DAL;
+using MixERP.HRM.DTO;
 
 namespace MixERP.HRM.Controllers.Tasks
 {
+    [AntiForgery]
     public class AttendanceController : DashboardController
     {
         [Route("dashboard/hrm/tasks/attendance")]
@@ -19,6 +23,15 @@ namespace MixERP.HRM.Controllers.Tasks
             var model = await Employees.GetEmployeesAsync(this.Tenant, meta.OfficeId).ConfigureAwait(true);
 
             return this.FrapidView(this.GetRazorView<AreaRegistration>("Tasks/Attendance/Index.cshtml", this.Tenant), model);
+        }
+
+        [Route("dashboard/hrm/tasks/attendance")]
+        [MenuPolicy]
+        [HttpPost]
+        public async Task<ActionResult> PutAsync(List<Attendance> model)
+        {
+            await Attendances.PostAsync(this.Tenant, model).ConfigureAwait(true);
+            return this.Ok();
         }
     }
 }
